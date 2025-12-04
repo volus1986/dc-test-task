c48196aea72d41f5b036c053524fc63572bb7bff
Volodymyr <volus1986@gmail.com>
2025-12-04 18:49:03 +0200

    create order product details

---
+++ app/orders/components/OrderDetails/components/OrderProduct.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import ProductStatusIcon from '@/app/components/ProductStatusIcon';
+import ButtonIcon from '@/assets/components/ButtonIcon';
+import trashIcon from '@/assets/icons/trash-gray.svg';
+
+export type Props = {
+    id: number;
+    status: string;
+    imageUrl: string;
+    title: string;
+    serialNumber: number | string;
+};
+
+export default function OrderProduct({
+    id,
+    status,
+    imageUrl,
+    title,
+    serialNumber,
+}: Props) {
+    const handleRemove = () => {
+        console.log(`remove product handler for product id: ${id}`);
+    };
+
+    return (
+        <div
+            className="
+                border-b border-[#cfd8dc]
+                px-10 py-3 flex items-center justify-items-stretch gap-8
+            "
+        >
+            <ProductStatusIcon status={status} />
+            <Image src={imageUrl} alt="Img" width={48} height={48} />
+            <div className="grow">
+                <p className="underline decoration-gray-500">{title}</p>
+                <p>{serialNumber}</p>
+            </div>
+            <ButtonIcon iconUrl={trashIcon} onClick={handleRemove} />
+        </div>
+    );
+}
