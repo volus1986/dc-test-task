2cdb7474b7ce49fe75cdb09df3d8e2df27c45b2a
Volodymyr <volus1986@gmail.com>
2025-12-04 18:44:22 +0200

    update order details component

--- app/orders/components/OrderDetails/index.tsx
+++ app/orders/components/OrderDetails/index.tsx
@@ -1,9 +1,9 @@
 import plusIcon from '@/assets/icons/plus-white.svg';
 import closeIcon from '@/assets/icons/x-gray.svg';
-
-function getProductsByOrderId(orderId: number) {
-    return [];
-}
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store';
+import OrderProduct from '@/app/orders/components/OrderDetails/components/OrderProduct';
+import { STATUSES } from '@/app/components/ProductStatusIcon';
 
 type Props = {
     orderId: number;
@@ -11,28 +11,47 @@ type Props = {
 };
 
 export default function OrderDetails({ orderId, onCloseCallback }: Props) {
+    const order = useSelector((state: RootState) =>
+        state.orders.find((order) => order.id === orderId),
+    );
+
+    const orderProducts = useSelector((state: RootState) =>
+        state.products.filter((product) => product.order === orderId),
+    );
+
+    const productElements = orderProducts.map((product) => {
+        return (
+            <OrderProduct
+                key={product.id}
+                id={product.id}
+                status={STATUSES.free}
+                imageUrl={product.photo}
+                title={product.title}
+                serialNumber={product.serialNumber}
+            />
+        );
+    });
+
     return (
-        <div className="p-8 border border-[#cfd8dc] rounded-sm relative w-[960px]">
+        <div className="border border-[#cfd8dc] border-b-0 rounded-sm relative w-[960px] h-fit bg-white">
             <button
                 className="absolute -right-6 -top-6 bg-white cursor-pointer w-11 h-11 flex items-center justify-center rounded-full shadow-[4px_4px_8px_rgba(0,0,0,0.25)]"
                 onClick={onCloseCallback}
             >
                 <img width={22} height={22} src={closeIcon.src} alt="close" />
             </button>
-            <div>
-                Order Details Component aslkdjf salgj sdlfgjh sdlfjg
-                al;sdjflasdjf lsadfkjg dskljfgh asldf jlkasjf lsdfg dlskdjf
-                lskdfgjdfghsjdfghklj{' '}
-            </div>
-            <div className="mt-6 flex items-center">
+            <h3 className="pt-8 px-8">{order?.title ?? ''}</h3>
+            <div className="mt-2 px-8 pb-2  flex items-center border-b border-b-[#f0f3f5]">
                 <button
                     className="w-4 h-4 flex items-center rounded-full cursor-pointer bg-[#8bc34a]"
                     onClick={onCloseCallback}
                 >
                     <img src={plusIcon.src} alt="add" color="#ff0000" />
                 </button>
-                <div className="ml-4">Add Product</div>
+                <div className="ml-4 text-[#8bc34a]">Add Product</div>
             </div>
+
+            {productElements}
         </div>
     );
 }
