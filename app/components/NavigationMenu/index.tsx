ed4d702332a328bbdf9dbd17c1f16392735dc0fa
Volodymyr <volus1986@gmail.com>
2025-12-05 15:32:21 +0200

    update nav menu styles

--- app/components/NavigationMenu/index.tsx
+++ app/components/NavigationMenu/index.tsx
@@ -5,7 +5,7 @@ export default function NavigationMenu() {
     return (
         <div
             className={`
-            flex-shrink-0 w-60 h-full bg-[#f0f3f5] mr-4
+            flex-shrink-0 w-60 h-full bg-[#f0f3f5]
             shadow-[12px_0_24px_-12px_rgba(0,0,0,0.25)]
         `}
         >
