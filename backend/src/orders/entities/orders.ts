b554ef6e987fb96aab006cc74a2e7edc77f67854
Volodymyr <volus1986@gmail.com>
2025-12-06 21:35:12 +0200

    move orders to BE

---
+++ backend/src/orders/entities/orders.ts
@@ -0,0 +1,29 @@
+export type Orders = {
+  id: number;
+  title: string;
+  date: string;
+  description: string;
+};
+
+export default [
+  {
+    id: 1,
+    title:
+      'Order 1 very long name example to check overflow askdjfhkasdfh gksajgdfh dksfjgh asdfg askjdfh saikjdfh difughsidufhg ksdjfh iuwe fiasdf hsakdfh iuse',
+    date: '2017-06-29 12:09:33',
+    description: 'desc',
+  },
+  {
+    id: 2,
+    title:
+      'Order 2 very long name example to check overflow giroe glkadf gkljdsfh gkdjfhg irueh gi',
+    date: '2017-06-29 12:09:33',
+    description: 'desc',
+  },
+  {
+    id: 3,
+    title: 'Order 3',
+    date: '2017-06-29 12:09:33',
+    description: 'desc',
+  },
+];
