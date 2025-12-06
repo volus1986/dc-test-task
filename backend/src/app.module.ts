3e7f60874b74c5b822bc771f3872c521480971d2
Volodymyr <volus1986@gmail.com>
2025-12-06 21:58:13 +0200

    init products backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app.gateway';
 import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [OrdersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService, AppGateway],
 })
