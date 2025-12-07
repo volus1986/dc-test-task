de994232391027aeead1500d99a1a808ea9bd016
Volodymyr <volus1986@gmail.com>
2025-12-07 13:01:34 +0200

    update FE deploy config

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -1,2 +1,4 @@
-export const BACKEND_URL =
-    process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:3001';
+const url = process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost';
+const port = process.env.BACKEND_SERVICE_PORT ?? '3001';
+
+export const BACKEND_URL = `${url}:${port}`;
