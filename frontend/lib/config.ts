0fa698923fd7dfe3266f6030a71e50fce6551b42
Volodymyr <volus1986@gmail.com>
2025-12-07 14:28:24 +0200

    fix prod config

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -1,4 +1,4 @@
 const url = process.env.NEXT_PUBLIC_BACKEND_SERVICE_URL ?? 'http://localhost';
 const port = process.env.NEXT_PUBLIC_BACKEND_SERVICE_PORT ?? '3001';
 
-export const BACKEND_URL = `${url}:${port}`;
+export const BACKEND_URL = port && !url.includes(':') ? `${url}:${port}` : url;
