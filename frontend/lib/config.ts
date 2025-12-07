47df66848e05297a32d8117b1cac0c581dec6c64
Volodymyr <volus1986@gmail.com>
2025-12-07 13:43:02 +0200

    update FE docker config for prod

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -1,4 +1,4 @@
-const url = process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost';
-const port = process.env.BACKEND_SERVICE_PORT ?? '3001';
+const url = process.env.NEXT_PUBLIC_BACKEND_SERVICE_URL ?? 'http://localhost';
+const port = process.env.NEXT_PUBLIC_BACKEND_SERVICE_PORT ?? '3001';
 
 export const BACKEND_URL = `${url}:${port}`;
